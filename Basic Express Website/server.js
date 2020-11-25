632c9773f6ff2c2dfa40c9c3aae613513907291c
Prashant Mudgal <prashantmudgal000@gmail.com>
2020-11-25 09:35:55 +0530

    Created simple website using Bootstrap.

--- Basic Express Website/server.js
+++ Basic Express Website/server.js
@@ -4,41 +4,43 @@ const path = require('path');
 const fs = require('fs');
 
 const mimeTypes = {
-	"html" : "text/html",
-	"jpeg" : "image/jpeg",
-	"jpg" : "image/jpg",
-	"png" : "image/png",
-	"js" : "text/javascript",
-	"css" : "text/css"
+	"html": "text/html",
+	"jpeg": "image/jpeg",
+	"jpg": "image/jpg",
+	"png": "image/png",
+	"js": "text/javascript",
+	"css": "text/css"
 };
-const server = http.createServer(function(req, res){
+
+http.createServer(function(req, res){
 	var uri = url.parse(req.url).pathname;
 	var fileName = path.join(process.cwd(), unescape(uri));
-	console.log("loading... "+uri);
+	console.log('Loading '+ uri);
 	var stats;
 
 	try{
 		stats = fs.lstatSync(fileName);
-	}
-	catch(e){
-		res.writeHead(404, {"Content-type": "text/plain"});
-		res.write("404 not found\n");
+	} catch(e){
+		res.writeHead(404, {'Content-type': 'text/plain'});
+		res.write('404 Not Found\n');
 		res.end();
 		return;
 	}
 
-	if (stats.isFile()){
-		var mimeType = mimeTypes[path.extname(filename).split(".").reverse()[0]];
+	if(stats.isFile()){
+		var mimeType = mimeTypes[path.extname(fileName).split(".").reverse()[0]];
 		res.writeHead(200, {'Content-type': mimeType});
+
 		var fileStream = fs.createReadStream(fileName);
 		fileStream.pipe(res);
-	}else if (stats.isDirectory()){
-		res.writeHead(302,{
-			'location':'index.html'
+	} else if(stats.isDirectory()) {
+		res.writeHead(302, {
+			'Location': 'index.html'
 		});
-	}else{
-		res.writeHead(500, {'Content-type': 'text/plain'});
-		res.write("500 Internal Error\n");
+		res.end();
+	} else {
+		res.writeHead(500, {'Content-type':'text/plain'});
+		res.write('500 Internal Error\n');
 		res.end();
 	}
 
